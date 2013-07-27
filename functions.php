f4d248bf832a2589b755b53e3d45a12988036fc6
Lauren McCarthy <laurmccarthy@gmail.com>
2013-07-27 11:54:01 -0700

    fix

--- functions.php
+++ functions.php
@@ -22,7 +22,15 @@
 		$qry = "INSERT INTO interactions(user, name, moments, rating) 
 		VALUES('$user', '$name', '$moments', '$rating')";
 		$result=mysql_query($qry);
+	}	
+
+	//Function to sanitize values received from the form. Prevents SQL injection
+	function clean($str) {
+		$str = @trim($str);
+		if(get_magic_quotes_gpc()) {
+			$str = stripslashes($str);
+		}
+		return mysql_real_escape_string($str);
 	}
-
-
+	
 ?>
--- submit.php
+++ submit.php
@@ -1,13 +1,24 @@
 <?php	
 
 	require_once("functions.php");
-
-	if (clean($_POST['type'] == "interaction") {
+	
+	if (true) {
+		$type = clean($_POST['type']);
 		$user = clean($_POST['user']);
 		$name = clean($_POST['name']);
 		$moments = clean($_POST['moments']);
 		$rating = clean($_POST['rating']);
 		
+	} else { // for testing
+		$type = clean($_GET['type']);
+		$user = clean($_GET['user']);
+		$name = clean($_GET['name']);
+		$moments = clean($_GET['moments']);
+		$rating = clean($_GET['rating']);
+	}
+	
+	
+	if ($type == "interaction") {
 		submit($user, $name, $moments, $rating);
 	}
 ?>
