9ceff553f5e847f35a004f32de13994b3abf724e
Lauren McCarthy <laurmccarthy@gmail.com>
2013-07-28 17:13:18 -0700

    updating get vs post

--- submit.php
+++ submit.php
@@ -1,24 +1,18 @@
 <?php	
 
 	require_once("functions.php");
-	
-	if (true) {
-		$type = clean($_POST['type']);
-		$user = clean($_POST['user']);
-		$name = clean($_POST['name']);
-		$moments = clean($_POST['moments']);
-		$rating = clean($_POST['rating']);
-		
-	} else { // for testing
-		$type = clean($_GET['type']);
-		$user = clean($_GET['user']);
-		$name = clean($_GET['name']);
-		$moments = clean($_GET['moments']);
-		$rating = clean($_GET['rating']);
-	}
+
+	$type = clean($_GET['type']);
+	$user = clean($_GET['user']);
+	$name = clean($_GET['name']);
+	$moments = clean($_GET['moments']);
+	$rating = clean($_GET['rating']);
+
 	
 	
 	if ($type == "interaction") {
 		submit($user, $name, $moments, $rating);
 	}
+	
+	//submit("testuser", "testname", "testmoments", 0);
 ?>
